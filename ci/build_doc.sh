ad934c22b7b4fb05caf427b840bbd4975c3bd68c
Adrian-Stanea <Adrian.Stanea@analog.com>
2025-02-26 14:27:08 +0200

    ci: add wrapper script for building documentation
    Signed-off-by: Adrian-Stanea <Adrian.Stanea@analog.com>

---
+++ ci/build_doc.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+BASE_PATH=$(git rev-parse --show-toplevel)
+
+echo "Installing dependencies"
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y graphviz doxygen
+
+echo "Generating the build directory"
+pushd "$BASE_PATH" >/dev/null || exit
+mkdir -p _build
+
+echo "Building the docs using rosdoc2"
+cd "$BASE_PATH/_build" || exit
+
+# Generate docs for each submodule
+for submodule in $(git submodule | awk '{ print $2 }' | xargs); do
+    echo "Submodule: $submodule"
+    rosdoc2 build --package-path "$submodule" --debug &
+done
+
+# wait for background jobs to finish
+wait
+
+# Generate docs fo the meta package which links to the submodules
+rosdoc2 build --package-path "$BASE_PATH/src/adi_ros2" --debug
+
+popd >/dev/null
